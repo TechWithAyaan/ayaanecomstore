9feb93e57bbe7310c83c29397464c0cf2398c833
TechWithAyaan <techwithayaanfaheem@gmail.com>
2025-08-03 03:03:15 -0700

    14 august sale

---
+++ app.js
@@ -0,0 +1,8 @@
+
+Swal.fire({
+  position: "top-end",
+  icon: "success",
+  title: "Your Order has been placed successfully",
+  showConfirmButton: false,
+  timer: 1500
+});
---
+++ app2.js
@@ -0,0 +1,8 @@
+
+Swal.fire({
+  position: "top-end",
+  icon: "success",
+  title: "Independence Day Sale is Live!",
+  showConfirmButton: false,
+  timer: 2500
+});
