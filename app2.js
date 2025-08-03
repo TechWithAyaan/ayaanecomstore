e18d15625d565757264991b12fcc420c9f7d028e
TechWithAyaan <techwithayaanfaheem@gmail.com>
2025-08-03 04:13:00 -0700

    Fake Pop Up

--- app2.js
+++ app2.js
@@ -6,3 +6,37 @@ Swal.fire({
   showConfirmButton: false,
   timer: 2500
 });
+  const popup = document.getElementById('sale-popup');
+  const popupText = popup.querySelector('.popup-text');
+  const popupImg = popup.querySelector('img');
+
+  const products = [
+    { name: "Wireless Earbuds", img: "https://img.drz.lazcdn.com/static/pk/p/f59eef6c3aec162c21197d04051bb242.jpg_400x400q75.avif" },
+    { name: "Smart Watch", img: "https://img.drz.lazcdn.com/static/pk/p/725e0b1f7cad6cee8992990a6aea4fad.jpg_400x400q75.avif" },
+    { name: "Bluetooth Speaker", img: "https://img.drz.lazcdn.com/static/pk/p/725e0b1f7cad6cee8992990a6aea4fad.jpg_400x400q75.avif" },
+    { name: "LED Ring Light", img: "https://img.drz.lazcdn.com/static/pk/p/725e0b1f7cad6cee8992990a6aea4fad.jpg_400x400q75.avif" },
+    { name: "Phone Case", img: "https://img.drz.lazcdn.com/g/kf/S7b5229cf92df4c2489340706f158b6a7H.jpg_400x400q75.avif" }
+  ];
+
+  const cities = ["Lahore", "Karachi", "Islamabad", "Rawalpindi", "Faisalabad"];
+
+  function showFakePopup() {
+    const randomProduct = products[Math.floor(Math.random() * products.length)];
+    const randomCity = cities[Math.floor(Math.random() * cities.length)];
+
+    popupText.innerHTML = `🛒 Someone in ${randomCity} just bought "${randomProduct.name}"!`;
+    popupImg.src = randomProduct.img;
+
+    popup.style.display = 'flex';
+
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 5000);
+  }
+
+  // Run randomly every 15 seconds
+  setInterval(() => {
+    if (Math.random() > 0.5) {
+      showFakePopup();
+    }
+  }, 11000);
